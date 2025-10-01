bdfb2239bc92aaa5c1154e1b63781d8a63ebd1d1
Lisan <lisa.lassi@live.se>
2025-10-01 17:27:45 +0200

    Add menu choice functionality and update menu options to include exit option

--- funk.py
+++ funk.py
@@ -6,5 +6,17 @@ def print_main_menu():
     print ("[4] Visa larm\n")
     print ("[5] Starta övervakningsläge\n")
     print ("[6] Ta bort alarm\n")
+    print ("[7] Avsluta programmet")
     print("---------------------------------------------------")
 
+def menu_choice():
+    while True:
+        menu_input = input("\nGör ett menyval 1-7: ")
+        try:
+            menu_number = int(menu_input)
+            if 1 <= menu_number <= 7:
+                return menu_choice
+            else:
+                print("Du måste välja en siffra mellan 1-7.")
+        except ValueError:
+            print("Ogiltigt val! Det måste vara en siffra.")
--- slutuppgift.py
+++ slutuppgift.py
@@ -3,19 +3,23 @@ import time
 
 def main():
     from funk import print_main_menu
-    menu_is_running = True
-
-    while menu_is_running:
+    from funk import menu_choice
+    
+    while True:
         print_main_menu()
 
+        menu_choice()
+
+        '''
         try:
-            menu_choice = int(input("Gör ett menyval 1-5: "))
-            if menu_choice < 1 or menu_choice > 5:
-                print("\nOgiltigt val! Du måste skriva en siffra mellan 1-5. Försök igen")
+            menu_choice = int(input("Gör ett menyval 1-7: "))
+            if menu_choice < 1 or menu_choice > 7:
+                print("\nOgiltigt val! Du måste skriva en siffra mellan 1-7. Försök igen!\n")
                 continue
         except:
-            print("\nOgiltigt val! Du måste ange en siffra mellan 1-5. Försök igen")
+            print("\nOgiltigt val! Du måste ange en siffra mellan 1-7. Försök igen!\n")
             continue
+        '''
 
         if menu_choice == 1:
             print()
@@ -31,6 +35,13 @@ def main():
 
         elif menu_choice == 5:
             print()
+
+        elif menu_choice == 6:
+            print()
+        
+        elif menu_choice == 7:
+            print("Avslutar programmet...")
+            break
 
 if __name__ == "__main__":
     main()
