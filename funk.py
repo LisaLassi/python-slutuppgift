d0153b7d4d857117ac767caf662f763da5ad58c1
Lisan <lisa.lassi@live.se>
2025-10-01 16:19:04 +0200

    Implement main menu functionality and add print_main_menu function

---
+++ funk.py
@@ -0,0 +1,10 @@
+def print_main_menu():
+    print("------------ Välkommen till programmet ------------\n")
+    print ("[1] Starta övervakning\n")
+    print ("[2] Lista aktiv övervakning\n")
+    print ("[3] Skapa larm\n")
+    print ("[4] Visa larm\n")
+    print ("[5] Starta övervakningsläge\n")
+    print ("[6] Ta bort alarm\n")
+    print("---------------------------------------------------")
+
--- slutuppgift.py
+++ slutuppgift.py
@@ -1,11 +1,36 @@
 import psutil
 import time
 
-print("------------ MENY ------------")
-print ("[1] Starta övervakning")
-print ("[2] Lista aktiv övervakning")
-print ("[3] Skapa larm")
-print ("[4] Visa larm")
-print ("[5] Starta övervakningsläge")
-print ("[6] Ta bort alarm")
-print("------------------------------")
+def main():
+    from funk import print_main_menu
+    menu_is_running = True
+
+    while menu_is_running:
+        print_main_menu()
+
+        try:
+            menu_choice = int(input("Gör ett menyval 1-5: "))
+            if menu_choice < 1 or menu_choice > 5:
+                print("\nOgiltigt val! Du måste skriva en siffra mellan 1-5. Försök igen")
+                continue
+        except:
+            print("\nOgiltigt val! Du måste ange en siffra mellan 1-5. Försök igen")
+            continue
+
+        if menu_choice == 1:
+            print()
+
+        elif menu_choice == 2:
+            print()
+
+        elif menu_choice == 3:
+            print()
+
+        elif menu_choice == 4:
+            print()
+
+        elif menu_choice == 5:
+            print()
+
+if __name__ == "__main__":
+    main()
